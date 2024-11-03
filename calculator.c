0793683008bf7d3d20373080e09b9738e67d1974
Nicolás Aguado <61473739+nicoagr@users.noreply.github.com>
2024-11-03 17:06:21 +0100

    feat: modular division

--- calculator.c
+++ calculator.c
@@ -16,6 +16,10 @@ int divide(int a, int b) {
 	return a / b;
 }
 
+int mod(int a, int b) {
+    return a % b;
+}
+
 int main() {
     int x, y;
     printf("Enter two integers: ");
@@ -24,10 +28,12 @@ int main() {
     printf("Sum: %d\n", add(x, y));
     printf("Difference: %d\n", subtract(x, y));
     printf("Product: %d\n", multiply(x, y));
-	if (x != 0)	
+	if (x != 0)	{
 		printf("Division: %d\n", divide(x, y));
+        printf("Modular Division: %d\n", mod(x, y));
+    }
 	else
-		printf("Division cannot be performed because b = 0\n");
+		printf("Division and modular division cannot be performed because b = 0\n");
 	
     return 0;
 }
